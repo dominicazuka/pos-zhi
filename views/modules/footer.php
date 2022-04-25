e47eaf2d0263d5c7d855ccff2bf69e3aa689bbff
visitdominicazuka <visitdominicazuka@gmail.com>
2022-04-25 11:22:55 +0100

    pos-zhi initial

---
+++ views/modules/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+
+    <strong>Copyright &copy; <?php
+                                echo date("Y");
+                                ?> <a href="http://zhi.ng" target="_blank">Zee Holiday Inn & Suite LTD</a>
+    </strong>
+
+    All rights reserved
+
+</footer>
